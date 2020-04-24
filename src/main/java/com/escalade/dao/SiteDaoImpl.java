88880c6db0ad1b0ae35fb54233d1ead2ba8e47e6
Brichet <brichet.b@hotmail.fr>
2020-04-24 15:36:16 +0200

    Suite du Usecase 1

--- src/main/java/com/escalade/dao/SiteDaoImpl.java
+++ src/main/java/com/escalade/dao/SiteDaoImpl.java
@@ -4,22 +4,20 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.List;
 
 @Stateless
 public class SiteDaoImpl {
     private static final String HQL_SELECT_SITE ="SELECT s FROM SiteEscalade s";
-    private static final String PARAM_SITE ="site";
 
     // Injection du manager, qui s'occupe de la connexion à la BDD
     @PersistenceContext( unitName = "SITE_ESCALADE")
     private EntityManager entityManager;
 
     // Afficher les sites d'escalade
-    public SiteEscalade listSites(){
-        SiteEscalade siteEscalade = null;
-        Query query=entityManager.createQuery(HQL_SELECT_SITE);
-            siteEscalade =(SiteEscalade) query.getResultList();
-
-        return siteEscalade;
+    public List<SiteEscalade> listSites()
+    {
+            Query query = entityManager.createQuery(HQL_SELECT_SITE,SiteEscalade.class);
+            return query.getResultList();
     }
 }
--- src/main/webapp/WEB-INF/site.jsp
+++ src/main/webapp/WEB-INF/site.jsp
@@ -1,9 +1,23 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
     <title>Site d'escalade</title>
 </head>
 <body>
-        <!-- Afficher dans un tableau les données de le BDD -->
+        <table>
+            <tr>
+                <th>id</th>
+                <th>Nom</th>
+                <th>Type</th>
+                <th>Secteur</th>
+                <th>Voie</th>
+                <th>Longueur</th>
+                <th>Cotation</th>
+                <th>Orientation</th>
+            </tr>
+
+            <c:forEach items="${param.}"
+        </table>
 </body>
 </html>
