bc16319dc0f445b9d8f9aa4f52f02321829e8d2e
Brichet <brichet.b@hotmail.fr>
2020-05-29 14:17:04 +0200

    début de la fonctionnalité qui permet d'ajouter un site

--- src/main/java/com/escalade/controller/SiteController.java
+++ src/main/java/com/escalade/controller/SiteController.java
@@ -4,12 +4,9 @@ import com.escalade.service.SiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.net.http.HttpRequest;
 
 /**
  * @Controller annotation pour les controlleurs classique, généralement utilisé en combinaison avec @RequestMapping
@@ -23,6 +20,34 @@ public class SiteController {
 
     @Autowired
     private SiteService siteService;
+
+    /**
+     * Méthode qui ajoute un site en base
+     * @param model
+     * @param nom
+     * @param departement
+     * @param pays
+     * @param nbreSecteur
+     * @param type
+     * @param cotationMin
+     * @param cotationMax
+     * @param longueurMax
+     * @return
+     */
+    @RequestMapping(value = "/addSite", method = RequestMethod.POST)
+    public String addSite ( Model model,
+                            @RequestParam(name = "nom", defaultValue = "", required = false) String nom,
+                            @RequestParam(name = "departement", defaultValue = "", required = false) String departement,
+                            @RequestParam(name = "pays", defaultValue = "", required = false) String pays,
+                            @RequestParam(name = "nbreSecteur", defaultValue = "", required = false) String nbreSecteur,
+                            @RequestParam(name = "type", defaultValue = "", required = false) String type,
+                            @RequestParam(name = "cotationMin", defaultValue = "0", required = false) int cotationMin,
+                            @RequestParam(name = "cotationMax", defaultValue = "0", required = false) int cotationMax,
+                            @RequestParam(name = "longueurMax", defaultValue = "", required = false) int longueurMax
+                            ){
+        
+        return "sites";
+    }
 
     /**
      *
--- src/main/webapp/WEB-INF/views/addSite.jsp
+++ src/main/webapp/WEB-INF/views/addSite.jsp
@@ -1,7 +1,46 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
 
 <body>
 
+    <form:form id="addSite" method="post" action="/addSites">
+
+        <div class="nom">
+            <label id="nom" class="col-md-3 control-label">Nom du site</label>
+        </div>
+        <div class="departement">
+            <label id="departement" class="col-md-3 control-label">Département du site</label>
+        </div>
+
+        <div class="pays">
+            <label id="pays" class="col-md-3 control-label">Pays du site</label>
+        </div>
+
+        <div class="nbreSecteur">
+            <label id="nbreSecteur" class="col-md-3 control-label">Nombre secteur du site</label>
+        </div>
+
+        <select name="type">
+            <div id="type">
+                <option value="Falaise">Falaise</option>
+                <option value="Bloc">Bloc</option>
+            </div>
+        </select>
+
+        <div class="cotationMin">
+            <label id="cotationMin" class="col-md-3 control-label">Cotation minimum du site</label>
+        </div>
+
+        <div class="cotationMax">
+            <label id="cotationMax" class="col-md-3 control-label">Cotation maximum du site</label>
+        </div>
+
+        <div class="longueurMax">
+            <label id="longueurMax" class="col-md-3 control-label">Longueur maximum du site</label>
+        </div>
+
+    </form:form>
+
 </body>
-</html>
+
